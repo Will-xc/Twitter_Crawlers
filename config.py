3b2d61209a915d6c14a6a5758d120133cb153d50
Will-xc <62529342+Will-xc@users.noreply.github.com>
2024-07-19 12:09:05 +0800

    Add files via upload
    补充上次未上传的配置文件

---
+++ config.py
@@ -0,0 +1,8 @@
+# coding=utf-8
+# @时间    :2024/3/2 15:26
+# @作者    :WIll
+# @功能    :文件的地址配置
+
+visitied_file = './profile/visited.txt'
+tweets_file = './data/data_keywords_100.csv'
+comments_file = './data/comments_keywords_100.json'
